f95246b6c381c37fcca4c77e15375c6b020f5034
ThibautG <thibaut.gaudinat@gmail.com>
2025-05-23 11:37:01 +0200

    div container + h1 + btn create dans layout

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>🎥FavMovies✨</title>
+    <title>FavMovies✨</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
@@ -12,8 +12,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
-<body class="bg-light text-dark d-flex flex-column align-items-center justify-content-lg-center p-4 p-lg-5 min-vh-100">
+<body class="bg-light text-dark d-flex flex-column align-items-center p-4 p-lg-5 min-vh-100">
+<div class="container">
 
-{{$slot}}
+    <h1>Movie Manager</h1>
+    <a href="{{ route('movies.create') }}" class="btn btn-primary mb-3">Create Movie</a>
+
+    {{$slot}}
+
+</div>
 </body>
 </html>
