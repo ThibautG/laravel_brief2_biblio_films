efdf1dc3dc2904cb190a11a5ff564e1ddccc7906
ThibautG <thibaut.gaudinat@gmail.com>
2025-05-23 11:23:00 +0200

    view layout + cdn bootstrap

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>🎥FavMovies✨</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body class="bg-light text-dark d-flex flex-column align-items-center justify-content-lg-center p-4 p-lg-5 min-vh-100">
+
+{{$slot}}
+</body>
+</html>
