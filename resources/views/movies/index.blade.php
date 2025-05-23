7ba11caf9f210d3cabf6c88fe3c4e9a47381ede5
ThibautG <thibaut.gaudinat@gmail.com>
2025-05-23 11:55:03 +0200

    création index

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,39 @@
+<x-layout>
+    {{--<div class="container">--}}
+
+
+        @if ($movies->count())
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th>Titre</th>
+                    <th>Année</th>
+                    <th>Note</th>
+                    <th>Commentaire</th>
+                    <th>Actions</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach ($movies as $movie)
+                    <tr>
+                        <td>{{ $movie->title }}</td>
+                        <td>{{ $movie->year }}</td>
+                        <td>{{ $movie->note }}</td>
+                        <td>{{ $movie->comment }}</td>
+                        <td>
+                            <a href="{{ route('tasks.edit', $movie->id) }}" class="btn btn-warning btn-sm">Edit</a>
+                            <form action="{{ route('tasks.destroy', $movie->id) }}" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>No movies available.</p>
+        @endif
+    {{--</div>--}}
+</x-layout>
